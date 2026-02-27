481ee11036905c3886586d902b477a59d9843f08
jaish <jaish3281@gmail.com>
2026-02-27 08:35:06 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker management system");
+        System.out.println("System instialised Correctly");
+    }
+}
