70623e23cbde9f99b9386415d4b2e36699f867bd
jaish <jaish3281@gmail.com>
2026-02-27 08:51:46 +0530

    UC2: Added hardcoded palindrome checker

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,6 +1,15 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker management system");
-        System.out.println("System instialised Correctly");
-    }
-}
+                String word = "jaish";
+                String reversed = "";
+
+                for(int i = word.length() - 1; i >= 0; i--) {
+                    reversed += word.charAt(i);
+                }
+                if(word.equals(reversed)) {
+                    System.out.println(word + " is a Palindrome.");
+                } else {
+                    System.out.println(word + " is NOT a Palindrome.");
+                }
+            }
+        }
